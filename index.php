9c4931182931c42e6fe3748105202476c085fdc6
Snowyuma <yuma82000.yuma82000@gmail.com>
2024-09-26 09:01:44 +0200

    ajout fichier css

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php
  echo 'Hello World';//test
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/main.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
