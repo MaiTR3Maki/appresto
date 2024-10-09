899b112d56616da987b083758b9cd45eeac8c049
MaiTR3Maki <lucas.ry31@gmail.com>
2024-10-09 12:56:22 +0200

    ajout du fichier js

---
+++ js/nav.js
@@ -0,0 +1,67 @@
+function admin() {
+    var x = document.getElementById("adminText");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function Questionnaire() {
+    var x = document.getElementById("Questionnaire");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+ }
+
+ function Dashboard() {
+    var x = document.getElementById("Dashboard");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+ }
+
+ function Dashboardpiechart() {
+    var x = document.getElementById("Dashboardpiechart");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+ }
+
+ function Feedback() {
+    var x = document.getElementById("Feedback");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+ }
+
+ function createlogin() {
+    var x = document.getElementById("createlogin");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+ }
+
+ function logout() {
+    var x = document.getElementById("logout");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+ }
+
+
+
+
+  
