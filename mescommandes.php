992f68c2cac860aa3b04fbb4ade16bf0459f4623
Snowyuma <yuma82000.yuma82000@gmail.com>
2024-10-17 11:57:56 +0200

    finalisation commandes

--- mescommandes.php
+++ mescommandes.php
@@ -17,33 +17,42 @@ check_session_user_non_connecte();
 <?php navbar();?>
 <h2>Mes Commandes Récentes</h2>
 <div class="commandes-container">
-<?php
-$sql = "SELECT id_commande, id_etat, _date, total_conso FROM commande";
-$result = $dbh->query($sql);
-$rows = $result->fetchAll(PDO::FETCH_ASSOC);
-if (count($rows) > 0) {
-    echo "<table>
-            <thead>
-                <tr>
-                    <th>Date</th>
-                    <th>Numéro de Commande</th>
-                    <th>État</th>
-                    <th>Total</th>
-                </tr>
-            </thead>
-            <tbody>";
-    foreach ($rows as $row) {
-        echo "<tr>
-                <td>{$row['_date']}</td>
-                <td>{$row['id_commande']}</td>
-                <td>{$row['id_etat']}</td>
-                <td>{$row['total_conso']}</td>
-              </tr>";
+<?php 
+$dbh = db_connect();
+print_r($_SESSION);
+if (isset($_SESSION['pseudo'])) {
+    $pseudo = $_SESSION['pseudo'];
+    $sql = "SELECT id_commande, id_etat, _date, total_conso FROM commande WHERE pseudo = :pseudo";
+    $stmt = $dbh->prepare($sql);
+    $stmt->bindParam(':pseudo', $pseudo, PDO::PARAM_INT);
+    $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    if (count($rows) > 0) {
+        echo "<table>
+                <thead>
+                    <tr>
+                        <th>Date</th>
+                        <th>Numéro de Commande</th>
+                        <th>État</th>
+                        <th>Total</th>
+                    </tr>
+                </thead>
+                <tbody>";
+        foreach ($rows as $row) {
+            echo "<tr>
+                    <td>{$row['_date']}</td>
+                    <td>{$row['id_commande']}</td>
+                    <td>{$row['id_etat']}</td>
+                    <td>{$row['total_conso']}</td>
+                  </tr>";
+        }
+        echo "</tbody>
+            </table>";
+    } else {
+        echo "Aucune commande trouvée.";
     }
-    echo "</tbody>
-        </table>";
 } else {
-    echo "Aucune commande trouvée.";
+    echo "Erreur : Utilisateur non connecté.";
 }
 ?>
 </div>
