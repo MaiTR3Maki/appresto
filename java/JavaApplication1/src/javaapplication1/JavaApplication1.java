08d639d88b1801d3b23e482f0f74e58e2975b242
Mathieu-Fraux <mathieu.fraux@limayrac.fr>
2025-03-13 08:39:13 +0100

    app java a supprimer

---
+++ java/JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author FM
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("coucouc :ffff");
+        NewJFrame fenetre = new NewJFrame();
+        fenetre.setVisible(true);
+        
+    }
+    
+}
