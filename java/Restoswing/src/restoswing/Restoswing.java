124515b92f484efac5fee144ce451f36425a7d7a
Mathieu-Fraux <mathieu.fraux@limayrac.fr>
2025-03-13 08:56:44 +0100

    java a supprimer 2

--- java/Restoswing/src/restoswing/Restoswing.java
+++ java/Restoswing/src/restoswing/Restoswing.java
@@ -1,24 +1,31 @@
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package javaapplication1;
+package restoswing;
 
 /**
  *
- * @author FM
+ * @author FM504305
  */
-public class JavaApplication1 {
+public class Restoswing {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("coucouc :ffff");
-        NewJFrame fenetre = new NewJFrame();
-        fenetre.setVisible(true);
         
+        //appeler API
+        
+        //Parser Json
+        
+        //alimenter la Jtable
+        
+        //affichage des fenétre
+        Liste_commande fenetre1 = new Liste_commande();
+        fenetre1.setVisible(true);
     }
     
 }
