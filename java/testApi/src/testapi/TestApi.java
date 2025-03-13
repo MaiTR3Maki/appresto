9832ebecab8d884bf44dde19b83c85c16673eebc
Mathieu-Fraux <mathieu.fraux@limayrac.fr>
2025-03-13 11:54:33 +0100

    avancée  3/13/2025

--- java/testApi/src/testapi/TestApi.java
+++ java/testApi/src/testapi/TestApi.java
@@ -8,6 +8,7 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import org.json.JSONObject;
 
 /**
  *
@@ -20,8 +21,35 @@ public class TestApi {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-     
-       
+
+        String json = ""; // Le JSON brut
+        String url = "http://localhost/projet/anne2/AP/appresto/appliweb/api/commandes_en_attente.php";
+// Créer un HttpClient
+        HttpClient client = HttpClient.newHttpClient();
+// Crée une requête HTTP GET
+        try {
+// Construit l'URL de la requête
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(new URI(url))
+                    .build();
+// Envoie la requête et attend la réponse
+            HttpResponse<String> response = client.send(request,
+                    HttpResponse.BodyHandlers.ofString());
+// Vérifie que la réponse est normale
+            if (response.statusCode() == 200) {
+                json = response.body();
+            } else {
+                System.err.println("Erreur : Code statut " + response.statusCode());
+            }
+        } catch (Exception ex) {
+            System.err.println("Erreur : " + ex.getMessage());
+//ex.printStackTrace();
+        }
+//System.out.println(json);
+
+        for (int i = 0; i < json.length(); i++) {
+            System.out.println("mathieu caca" + i);
+        }
 
     }
 
